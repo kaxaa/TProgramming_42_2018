a33c09ed81545da67cd7b4454db6b91bdbd8eb29
pavuk <43777313+pawaebawy@users.noreply.github.com>
2018-10-05 13:36:31 +0400

    message (#3)

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,7 +6,7 @@ namespace CourseApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Горбачёв Павел Сергеевич");
         }
     }
 }
